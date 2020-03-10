dd13003194e62fcebca1af8a6b01a62867ab6935
Janio Isacura <janioisacura@gmail.com>
2020-03-09 23:29:49 -0300

    e hacen pruebas para modificar estado

--- todolist.js
+++ todolist.js
@@ -10,7 +10,20 @@ switch(accion) {
         }
     break;
     case 'modificar' :
-        console.log();
+        const estadoArr = tareas.leer();
+        if (process.argv[3] !== undefined) {
+            for (let i = 0; i < estadoArr.length; i++) {
+                let estado = chalk.green(estadoArr[process.argv[3]].estado);
+                let nuevoEstado = chalk.green(process.argv[4]);
+                console.log((i + 1) + ' - ' + 'Descripción: ' + chalk.cyan(estadoArr[i].descripcion) + ' Estado ' + estado.replace(estadoArr[process.argv[3]].estado, nuevoEstado));
+                // console.log(estado);
+                
+            }
+        } else {
+            console.log('No hay nada para modificar');
+            
+        }
+        
     break;
     case undefined :
         console.log('Chino no me come esto cosa');
