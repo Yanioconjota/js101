a1f7a7c5bb7780fa3c6326b43ebe6f57b38917f5
nicolas <lopez>
2020-03-09 22:00:09 -0300

    Se actualizaron ejemplos y se importó librería

---
+++ tasks.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+const tareas = {
+    archivo: 'tasks.json',
+    leer: function() {
+        return JSON.parse(fs.readFileSync(this.archivo, 'utf-8'));
+    }
+}
+
+module.exports = tareas;
--- todolist.js
+++ todolist.js
@@ -1,10 +1,22 @@
-const tareas = require('./tasks.json');
-
+const tareas = require('./tasks');
+const chalk = require('chalk');
 const accion = process.argv[2];
 
-if(accion === 'listar') {
-    console.log('El usuario quiere listar');
-} else {
-    console.log('El usuario quiere ' + accion);
+switch(accion) {
+    case 'listar' : 
+        const tareasArray = tareas.leer();
+        for (let i = 0; i < tareasArray.length; i++) {
+            console.log((i+1) + ' - ' + 'Descripción: ' + chalk.cyan(tareasArray[i].descripcion) + ' Estado ' + chalk.green(tareasArray[i].estado));
+        }
+    break;
+    case 'modificar' :
+        console.log();
+    break;
+    case undefined :
+        console.log('Chino no me come esto cosa');
+        break;
+    default :
+        console.log('El usuario quiere ' + accion);
+    break;
 }
 
