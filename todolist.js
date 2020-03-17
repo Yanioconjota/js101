bf7880c8e3d62ccc6ac6e9069efb652cb5d92e8b
Janio Isacura <janioisacura@gmail.com>
2020-03-16 21:02:11 -0300

    Se modífico forEach

--- todolist.js
+++ todolist.js
@@ -5,8 +5,8 @@ const accion = process.argv[2];
 switch (accion) {
   case 'listar':
     const tareasArray = tareas.leer();
-    tareasArray.forEach((value, i) => {
-      console.log((i + 1) + ' - ' + 'Descripción: ' + chalk.cyan(tareasArray[i].descripcion) + ' Estado ' + chalk.green(tareasArray[i].estado));
+    tareasArray.forEach((tarea, i) => {
+      console.log((i + 1) + ' - ' + 'Descripción: ' + chalk.cyan(tarea.descripcion) + ' Estado ' + chalk.green(tarea.estado));
     });
     // for (let i = 0; i < tareasArray.length; i++) {
     //     console.log((i+1) + ' - ' + 'Descripción: ' + chalk.cyan(tareasArray[i].descripcion) + ' Estado ' + chalk.green(tareasArray[i].estado));
@@ -19,8 +19,6 @@ switch (accion) {
         let estado = chalk.green(estadoArr[process.argv[3]].estado);
         let nuevoEstado = chalk.green(process.argv[4]);
         console.log((i + 1) + ' - ' + 'Descripción: ' + chalk.cyan(estadoArr[i].descripcion) + ' Estado ' + estado.replace(estadoArr[process.argv[3]].estado, nuevoEstado));
-        // console.log(estado);
-
       }
     } else {
       console.log('No hay nada para modificar');
