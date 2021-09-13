a6074d71f479fbed660bf3a8680f702bb7324dd2
Janio Isacura <janioisacura@gmail.com>
2021-09-13 10:43:16 -0300

    Se agregaron ejemplos y config

---
+++ index.js
@@ -0,0 +1,7 @@
+let title = 'breaking bad rules!'
+
+//console.log(title.slice(-10));
+
+let nombreCompleto = (nombre, apellido) => `${nombre} ${apellido}`;
+let saludar = (nombre, apellido, callback) => `Hola ${callback(nombre, apellido)}`;
+console.log(saludar('Homero', 'Tompson', nombreCompleto));
